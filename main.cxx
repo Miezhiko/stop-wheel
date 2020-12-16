502d5f913bbbdf109e84401e94d8f5f98c943302
Qeenon <codemasha@gmail.com>
2020-12-16 14:15:32 +0400

    better way to stop mouse wheel on windows

---
+++ main.cxx
@@ -0,0 +1,20 @@
+#include <Windows.h>
+
+HHOOK mh;
+
+LRESULT __stdcall Callback(int c, WPARAM p, LPARAM l) {
+  if (c >= 0 && p == WM_MOUSEWHEEL) {
+    return 1;
+  }
+  return CallNextHookEx(mh, c, p, l);
+}
+
+int WINAPI WinMain(HINSTANCE i, HINSTANCE pi, LPSTR l, int s) {
+  mh = SetWindowsHookEx(WH_MOUSE_LL, Callback, NULL, 0);
+  MSG msg;
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return static_cast<int>(msg.wParam);
+}
